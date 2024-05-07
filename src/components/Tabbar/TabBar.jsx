ecef1a419cbb7def076cf4bad7425d2e8749ffa8
Dino9711 <dioniciorobles100@gmail.com>
2024-05-07 14:32:16 -0700

    chore: Refactor TabBar component and remove unused code

--- src/components/Tabbar/TabBar.jsx
+++ src/components/Tabbar/TabBar.jsx
@@ -1,21 +1,8 @@
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { Avatar, Card, Menu, MenuItem, Stack, Typography } from '@mui/material';
 import MuiAppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Toolbar from '@mui/material/Toolbar';
 import { styled, useTheme } from '@mui/material/styles';
-import { IconDeviceDesktop } from '@tabler/icons-react';
 import * as React from 'react';
-import { NavLink, Route, Switch, useHistory } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import authProvider from '../../auth/authProvider';
 import { CustomerScreen } from '../../screens/Customer/CustomerScreen';
 import { UserScreen } from '../../screens/User/UserScreen';
@@ -117,169 +104,19 @@ export const TabBar = () => {
 
   return (
     <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position='fixed'
-        sx={{ backgroundColor: '#ffffff' }}
-        elevation={0}
-        open={open}
-      >
-        <Toolbar
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            height: '5vh',
-          }}
-        >
-          <Stack
-            direction='row'
-            sx={{
-              display: 'flex',
-              alignContent: 'center',
-              alignItems: 'center',
-              height: '100%',
-            }}
-          >
-            {/* {logged && (
-              <IconButton
-                color='warning'
-                onClick={handleDrawerOpen}
-                edge='end'
-                sx={{ mr: 2, ...(open && { display: 'none' }) }}
-              >
-                <MenuIcon />
-              </IconButton>
-            )} */}
-            <Box
-              sx={{
-                display: 'flex',
-                alignContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-                width: 180,
-                borderColor: 'primary.main',
-                zIndex: -30,
-              }}
-            >
-              {/* <Box
-                component='img'
-                src={tabbarLogo}
-                sx={{
-                  height: '100%',
-                  width: '100%',
-                }}
-              /> */}
-              <Typography
-                variant='h6'
-                noWrap
-                sx={{
-                  color: '#242422',
-                  fontFamily: 'Futura',
-                }}
-              >
-                CASHBACKY
-              </Typography>
-            </Box>
-          </Stack>
-          {logged && (
-            <>
-              <IconButton onClick={handleUserMenu}>
-                <Avatar alt='Usuario' />
-              </IconButton>
-              <Menu
-                id='menu-appbar'
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleCloseUserMenu}
-              >
-                <MenuItem
-                  onClick={() => {
-                    setAnchorEl(null);
-                    window.localStorage.removeItem('username');
-                    authProvider.logout();
-                    history.push('/login');
-                  }}
-                >
-                  Cerrar sesión
-                </MenuItem>
-              </Menu>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant='persistent'
-        anchor='left'
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <List>
-          <NavLink
-            to='/maquinas'
-            style={{ textDecoration: 'none', color: 'black' }}
-          >
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <IconDeviceDesktop />
-                </ListItemIcon>
-                <ListItemText primary={'PRODUCTOS'} />
-              </ListItemButton>
-            </ListItem>
-          </NavLink>
-        </List>
-      </Drawer>
-      <Main open={open}>
-        <DrawerHeader />
-        <Card
-          elevation={0}
-          sx={{
-            borderRadius: '15px',
-            height: '100%',
-            backgroundColor: '#eef2f6',
-            minHeight: '85vh',
-          }}
-        >
-          <Switch>
-            {logged ? (
-              <>
-                <Route exact path='/'>
-                  {user_type === 'USER' ? <UserScreen /> : <CustomerScreen />}
-                </Route>
-              </>
-            ) : (
-              <Route exact path='/login'>
-                <Login />
-              </Route>
-            )}
-          </Switch>
-        </Card>
-      </Main>
+      <Switch>
+        {logged ? (
+          <>
+            <Route exact path='/'>
+              {user_type === 'USER' ? <UserScreen /> : <CustomerScreen />}
+            </Route>
+          </>
+        ) : (
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+        )}
+      </Switch>
     </Box>
   );
 };
