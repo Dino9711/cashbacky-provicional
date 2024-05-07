f3b5cc848e405d44c15a560d635134b65e183052
Dino9711 <dioniciorobles100@gmail.com>
2024-05-07 14:42:01 -0700

    feat: Add PromotionsComponent to CustomerScreen

---
+++ src/screens/Customer/components/PromotionsComponent.jsx
@@ -0,0 +1,54 @@
+import { Box, Typography } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'sonner';
+import { URL_SERVER } from '../../../helpers/ProviderUrl';
+
+export const PromotionsComponent = () => {
+  const [ads, setAds] = useState([]);
+
+  const getAds = async () => {
+    try {
+      const response = await axios.get(`${URL_SERVER}ads`);
+      if (response.data.ok) {
+        setAds(response.data.data);
+        toast.success('Promotions loaded');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Error loading promotions');
+    }
+  };
+
+  useEffect(() => {
+    getAds();
+  }, []);
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          marginBottom: '50px',
+        }}
+      >
+        {ads.length > 0 ? (
+          ads.map((ad) => <SingleAdComponent key={ad._id} data={ad} />)
+        ) : (
+          <Typography
+            align='center'
+            sx={{
+              width: '96vw',
+            }}
+            variant='h5'
+          >
+            No promotions available
+          </Typography>
+        )}
+      </Box>
+    </>
+  );
+};
