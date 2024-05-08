8895a12876917d193c4a74f95594cc1b785d61c3
Dino9711 <dioniciorobles100@gmail.com>
2024-05-08 09:12:25 -0700

    chore: Update background color in SingleAdComponent, SingleBranchComponent, SingleCardComponent, and SingleTransactionComponent

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,29 @@
+import { ThemeProvider, createTheme } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#222224',
+    },
+    secondary: {
+      main: '#4d8f8d',
+    },
+    success: {
+      main: '#4caf50',
+    },
+    error: {
+      main: '#f44336',
+    },
+    warning: {
+      main: '#ff9800',
+    },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+);
