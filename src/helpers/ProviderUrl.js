ad5454212ffe857be62e94284953eb2bd635987b
Dino9711 <dioniciorobles100@gmail.com>
2024-05-23 10:51:49 -0700

    chore: Update ProviderUrl.js to use production server URL

--- src/helpers/ProviderUrl.js
+++ src/helpers/ProviderUrl.js
@@ -1,3 +1,5 @@
-// export const URL_SERVER = 'https://wallet-services.dimosoft.dev/api/';
-export const URL_SERVER = 'http://localhost:8880/api/';
-export const SOCKET_URL = 'http://localhost:8880';
+export const URL_SERVER = 'https://wallet-services.dimosoft.dev/api/';
+export const SOCKET_URL = 'https://wallet-services.dimosoft.dev';
+
+// export const URL_SERVER = 'http://localhost:8880/api/';
+// export const SOCKET_URL = 'http://localhost:8880';
