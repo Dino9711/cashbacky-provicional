3b09cfd007b6f535232550816398ab99924ae1a0
Dino9711 <dioniciorobles100@gmail.com>
2024-05-31 12:16:56 -0700

    chore: Remove unused code and fix login redirect issue

--- src/components/Tabbar/TabBar.jsx
+++ src/components/Tabbar/TabBar.jsx
@@ -1,78 +1,25 @@
-import MuiAppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import { styled, useTheme } from '@mui/material/styles';
 import * as React from 'react';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import authProvider from '../../auth/authProvider';
 import { CustomerScreen } from '../../screens/Customer/CustomerScreen';
-import { Login } from '../../screens/login/Login';
 import { StoreComponent } from '../../screens/Marketplace/Store/StoreComponent';
+import { SuccessPaymentComponent } from '../../screens/Payments/SuccessPaymentComponent';
 import { UserScreen } from '../../screens/User/UserScreen';
-
-const drawerWidth = 165;
-
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(2),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    }),
-  }),
-);
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
+import { Login } from '../../screens/login/Login';
 
 export const TabBar = () => {
-  const theme = useTheme();
   const user = JSON.parse(window.localStorage.getItem('username'));
-  const [open, setOpen] = React.useState(false);
-  const [anchorEl, setAnchorEl] = React.useState(null);
   const history = useHistory();
   let [logged] = authProvider.useAuth();
 
   const user_type = window.localStorage.getItem('user_type');
 
-  React.useEffect(() => {
-    if (!logged && !user) {
-      history.push('/login');
-    } else {
-      history.push('/');
-    }
-  }, [logged, user, history]);
+  // React.useEffect(() => {
+  //   if (!logged && !user) {
+  //     history.push('/login');
+  //   }
+  // }, [logged, user, history]);
 
   React.useEffect(() => {
     if (JSON.parse(window.localStorage.getItem('user_data')) === null) {
@@ -81,28 +28,6 @@ export const TabBar = () => {
     }
   }, []);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const handleUserMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorEl(null);
-  };
-
-  React.useEffect(() => {
-    if (!logged && !user) {
-      history.push('/login');
-    }
-  }, [logged, user, history]);
-
   return (
     <Box
       sx={{
@@ -110,15 +35,20 @@ export const TabBar = () => {
       }}
     >
       <Switch>
-        {logged ? (
+        {logged && user_type === 'CUSTOMER' ? (
           <>
-            <Route exact path='/'>
-              {user_type === 'USER' ? <UserScreen /> : <CustomerScreen />}
-            </Route>
             <Route path='/marketplace/branches/:branchId'>
               <StoreComponent />
             </Route>
+            <Route exact path='/success-payment'>
+              <SuccessPaymentComponent />
+            </Route>
+            <Route exact path='/'>
+              <CustomerScreen />
+            </Route>
           </>
+        ) : user_type === 'USER' ? (
+          <UserScreen />
         ) : (
           <Route exact path='/login'>
             <Login />
